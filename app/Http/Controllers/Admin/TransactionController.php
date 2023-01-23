ef87943d53e88d77ae85228ca1af1b8f71e30540
Sajid Javed <engr.maliksajidkhan@gmail.com>
2023-01-23 11:34:03 +0400

    edit transaction

--- app/Http/Controllers/Admin/TransactionController.php
+++ app/Http/Controllers/Admin/TransactionController.php
@@ -13,6 +13,7 @@ use Gate;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Yajra\DataTables\Facades\DataTables;
+use Illuminate\Support\Facades\Auth;
 
 class TransactionController extends Controller
 {
@@ -80,6 +81,22 @@ class TransactionController extends Controller
 
     public function store(StoreTransactionRequest $request)
     {
+
+        $transaction_type = $request->transaction_type;
+        $bank_id = $request->bank_id;
+        $amount = $request->amount;
+
+        $bank = Bank::find($bank_id);
+        if($transaction_type=="Withdrawal"){
+            $request->request->add(['status' => 'Approved']);
+            $bank->balance = $bank->balance - $amount;
+        }else if($transaction_type=="Deposit"){
+            $request->request->add(['status' => 'Pending']);
+            $bank->balance = $bank->balance + $amount; 
+        }
+
+        $bank->save();
+
         $transaction = Transaction::create($request->all());
 
         return redirect()->route('admin.transactions.index');
@@ -104,7 +121,7 @@ class TransactionController extends Controller
         $transaction_type = $transaction->transaction_type;
         $bank_id = $transaction->bank_id;
         $amount = $transaction->amount;
-        $status = $transaction->status;
+        $status = $request->status;
 
         $bank = Bank::find($bank_id);
         if($transaction_type=="Withdrawal"){
@@ -114,28 +131,31 @@ class TransactionController extends Controller
         }
         $bank->save();
 
-
-        if($status=="Approved" || $status=="Pending"){
         // do new operation 
-        $transaction_type = $request->transaction_type;
-        $bank_id = $request->bank_id;
-        $amount = $request->amount;
+        if($status=="Approved" || $status=="Pending"){
+            $transaction_type = $request->transaction_type;
+            $bank_id = $request->bank_id;
+            $amount = $request->amount;
 
-        $bank = Bank::find($bank_id);
-        if($transaction_type=="Withdrawal"){
-            $bank->balance = $bank->balance - $amount;
-        }else if($transaction_type=="Deposit"){
-            $bank->balance = $bank->balance + $amount; 
+            $bank = Bank::find($bank_id);
+            if($transaction_type=="Withdrawal"){
+                $bank->balance = $bank->balance - $amount;
+            }else if($transaction_type=="Deposit"){
+                $bank->balance = $bank->balance + $amount; 
+            }
+
+            $bank->save();
         }
 
-        $bank->save();
-    }
+        if($status=="Approved" || $status=="Void"){
+            $request->request->add(['approver' => Auth::user()->name]);
+            $request->request->add(['approve_datetime' => now()]);
+        }
 
+            $transaction->update($request->all());
 
-        $transaction->update($request->all());
-
-        return redirect()->route('admin.transactions.index');
-    }
+            return redirect()->route('admin.transactions.index');
+        }
 
     public function show(Transaction $transaction)
     {
@@ -162,7 +182,6 @@ class TransactionController extends Controller
         }
 
         $bank->save();
-
         $transaction->delete();
 
         return back();
@@ -170,7 +189,24 @@ class TransactionController extends Controller
 
     public function massDestroy(MassDestroyTransactionRequest $request)
     {
-        Transaction::whereIn('id', request('ids'))->delete();
+        foreach(request('ids') as $id){
+            $transaction = Transaction::find($id);
+            $transaction_type = $transaction->transaction_type;
+            $amount = $transaction->amount;
+            $bank_id = $transaction->bank_id;
+
+            $bank = Bank::find($bank_id);
+            $bank = Bank::find($bank_id);
+
+            if($transaction_type=="Withdrawal"){
+                $bank->balance = $bank->balance + $amount;
+            }else if($transaction_type=="Deposit"){
+                $bank->balance = $bank->balance - $amount; 
+            }
+
+            $bank->save();
+            $transaction->delete();
+        }
 
         return response(null, Response::HTTP_NO_CONTENT);
     }
